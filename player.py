c54446b036709fe642c2edf6c9711f80e59870ca
Brian Weber <brianweber2@gmail.com>
2016-11-30 11:23:20 -0800

    Added function to validating player input.

--- player.py
+++ player.py
@@ -1,53 +1,69 @@
-from battleship import clear_screen, SHIP_INFO, BOARD_SIZE
+from battleship import clear_screen
 from board import Board
+from ship import Ship
 
 
-class Player(Board):
-	'''
-	Does the following to set the player's data:
+class Player(Board, Ship):
+    '''
+    Does the following to set the player's data:
 
-	1. Prompt the players for their names
-	2. Prompt user to place a ship
-	'''
-	def __init__(self, name):
-		Board.__init__(self)
-		self.name = name
+    1. Prompt the players for their names
+    2. Prompt user to place a ship
+    '''
+    def __init__(self, name):
+        Board.__init__(self)
+        Ship.__init__(self)
 
-
-	def __str__(self):
-		return "{}".format(self.name)
+        self.name = name
 
 
-	def prompt_user_to_place_ship(self):
-		self.user_choices = {}
-		for ship in SHIP_INFO:
-			clear_screen()
-			self.print_board(self.board)
-			print("{} choose ship locations".format(self.name))
-			# Validate user input
-			try:
-				self.user_choices[ship[0]] = (input("Place the location of the"
-				" {} ({} spaces): ".format(*ship))).upper().strip()
-				if set('[~!@#$%^&*()_+{}":;\']+$').intersection(
-					self.user_choices[ship[0]]) or (self.user_choices[ship[0]] not
-					in [key for row in self.board for key in row]):
-					raise ValueError	
-			except:
-				clear_screen()
-				print("Error! {} is not a valid input. Make sure you only use "
-					"lower or uppercase characters for columns and a number "
-					"1-{} for rows. Spaces are allowed.".format(
-						self.user_choices[ship[0]], BOARD_SIZE)
-					)
-				self.print_board(self.board)
-				self.user_choices[ship[0]] = (input("Place the location of the"
-				" {} ({} spaces): ".format(*ship))).upper().strip()
-
-			if input("Is it horizontal? (Y)/N: ").lower() != 'y':
-				self.user_choices[ship[0] + "_horizontal?"] = 'n'
-			# Place ship on board
-			self.place_ship(ship[0], self.user_choices[ship[0]], ship[1], 
-							self.user_choices[ship[0] + "_horizontal?"])
-			
+    def __str__(self):
+        return "{}".format(self.name)
 
 
+    def prompt_user_to_place_ship(self, ships):
+        self.user_choices = {}
+        print("{} choose ship locations".format(self.name))
+        for ship in ships:
+            self.user_choices[ship[0]] = (input("Place the location of the"
+            " {} ({} spaces): ".format(*ship))).upper().strip()
+            # Validate player input
+            self.user_choices[ship[0]] = self.validate_player_input(
+                ship, self.user_choices[ship[0]])
+
+            if input("Is it horizontal? (Y)/N: ").lower() != 'y':
+                self.user_choices[ship[0] + "_horizontal?"] = 'n'
+            else:
+                self.user_choices[ship[0] + "_horizontal?"] = 'y'
+            # Place ship on board
+
+            # Print updated board with ship location
+            # self.update_board()
+            clear_screen()
+            self.print_board()
+            print("{} choose ship locations".format(self.name))
+        return self.user_choices
+
+
+    def validate_player_input(self, ship, player_input):
+        """
+        Function validates player input for ship location.
+        """
+        input_validated = False
+        while input_validated == False:
+            if set('[~!@#$%^&*()_+{}":;\']+$').intersection(
+            player_input) or (player_input not in 
+            [letter+str(n) for n in range(1,11) for letter in "ABCDEFGHIJ"]):
+                clear_screen()
+                print("Error! {} is not a valid input. Make sure you only use "
+                        "lower or uppercase characters for columns and a number "
+                        "1-{} for rows. Spaces are allowed.".format(
+                        player_input, self.board_size)
+                )
+                self.print_board()
+                print("{} choose ship locations".format(self.name))
+                player_input = (input("Place the location of the"
+                " {} ({} spaces): ".format(*ship))).upper().strip()
+            else:
+                input_validated = True
+        return player_input
--- ship.py
+++ ship.py
@@ -1,12 +1,32 @@
-from battleship import SHIP_INFO
-
-
 class Ship:
-	'''
-	Everything to do with the ship itself
+    '''
+    Everything to do with the ship itself
 
-	Knows how to mark himself or piece of him as a hit.
-	Knows how to turn into a destroyed ship.
-	Knows how to return his size/dimensions.
-	'''
-	pass
+    Knows how to mark himself or piece of him as a hit.
+    Knows how to turn into a destroyed ship.
+    Knows how to return his size/dimensions.
+    '''
+    SHIP_INFO = [
+        ("Aircraft Carrier", 5),
+        ("Battleship", 4),
+        ("Submarine", 3),
+        ("Cruiser", 3),
+        ("Patrol Boat", 2)
+    ]
+
+    def __init__(self):
+        self.ship_info = self.SHIP_INFO
+
+
+    def return_ship_size(self, ship_name):
+        for ship in self.ship_info:
+            if ship[0] == ship_name:
+                return ship[1]
+
+
+    def ship_destoyed(self):
+        pass
+
+
+    def hit_or_miss(self):
+        pass
